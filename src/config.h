2e2cd060860229fc67640ff1f172bc470f8b5ad1
madhukartemba <madhusmiles.madhukar@gmail.com>
2025-10-01 00:08:48 +0530

    add version 3

--- src/config.h
+++ src/config.h
@@ -52,4 +52,30 @@
 
 #endif  // USE_VERSION_1
 
+#ifdef USE_VERSION_3
+
+// Display type
+#    define DISPLAY_TYPE DisplayType::SH1106
+#    define DISPLAY_FLIPPED false
+
+// Pin definitions
+#    define TOUCH_BUTTON_PIN D0
+#    define OLED_SCL_PIN D1
+#    define OLED_SDA_PIN D2
+#    define SPEAKER_PIN D3
+#    define IR_RX_PIN D5
+#    define IR_TX_PIN D6
+#    define NEOPIXEL_PIN D7
+
+// LED configuration
+#    define NUM_LEDS 30  // Number of LEDs in the circular strip
+#    define CENTER_LED 15
+
+// WiFi configuration
+#    define WIFI_AP_NAME "IRHub V3 Setup"
+#    define WIFI_AP_TIMEOUT 180      // 3 minutes timeout
+#    define WIFI_CONNECT_TIMEOUT 60  // 1 minute timeout
+
+#endif  // USE_VERSION_3
+
 #endif  // CONFIG_H
