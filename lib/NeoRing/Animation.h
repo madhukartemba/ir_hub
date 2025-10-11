51b09569e0044fb45f78681531aa57f76951192a
madhukartemba <madhusmiles.madhukar@gmail.com>
2025-10-11 17:18:47 +0530

    new ring library animation class

---
+++ lib/NeoRing/Animation.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <algorithm>  // std::fill
+#include <cstddef>    // for size_t
+#include <cstdint>    // for uint32_t
+#include <vector>     // for std::vector
+
+class Animation {
+   protected:
+    size_t ledCount = 0;
+    std::vector<uint32_t> buffer;
+
+   public:
+    virtual ~Animation() = default;
+
+    virtual void update() = 0;
+
+    virtual const std::vector<uint32_t>& getFrame() const { return buffer; }
+
+    virtual void reset() { std::fill(buffer.begin(), buffer.end(), 0); }
+};
